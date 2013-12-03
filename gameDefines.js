68030b937886429a7a02270fb9294612e116018d
Jonas Olmstead <sanojo@hotmail.com>
2013-12-03 12:06:07 +0100

    -added equipment

---
+++ gameDefines.js
@@ -0,0 +1,63 @@
+window.GAME = {
+	EQUIPMENT: {
+		shirt: {
+			classes: ['fighter'],
+			defense: [3, 6, 12, 24, 48],
+			offense: [0, 0, 0, 0, 0],
+			slot: 0,
+			z: 110
+		},
+		pants: {
+			classes: ['fighter'],
+			defense: [1, 2, 4, 8, 16],
+			offense: [0, 0, 0, 0, 0],
+			slot: 1,
+			z: 110
+		},
+		helm: {
+			classes: ['fighter'],
+			defense: [1, 2, 4, 8, 16],
+			offense: [0, 0, 0, 0, 0],
+			slot: 2,
+			z: 110
+		},
+		boots: {
+			classes: ['fighter'],
+			defense: [1, 2, 4, 8, 16],
+			offense: [0, 0, 0, 0, 0],
+			slot: 3,
+			z: 110
+		},
+		shield: {
+			classes: ['fighter'],
+			defense: [4, 8, 16, 32, 64],
+			offense: [0, 0, 0, 0, 0],
+			slot: 4,
+			z: 110
+		},
+		sword: {
+			classes: ['fighter'],
+			defense: [0, 0, 0, 0, 0],
+			offense: [2, 4, 8, 16, 32],
+			slot: 5,
+			z: 90
+		},
+		robe: {
+			classes: ['wizard'],
+			defense: [2, 3, 5, 8, 12],
+			offense: [2, 4, 8, 16, 32],
+			slot: 0,
+			z: 110
+		},
+		hat: {
+			classes: ['wizard'],
+			defense: [1, 2, 3, 4, 5],
+			offense: [1, 2, 4, 6, 10],
+			slot: 0,
+			z: 110
+		}
+	}
+
+
+
+}
